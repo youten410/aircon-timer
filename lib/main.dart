88213a6103c58baa0cac54fae52c05ef7f59cbe4
Shun Sato <techregisteremail@gmail.com>
2023-06-26 01:05:59 +0900

    update : time pickerと現在時刻の表示を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:async'; // 追加
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,13 +16,42 @@ class MyApp extends StatelessWidget {
 }
 
 class AirConTimer extends StatefulWidget {
-  const AirConTimer({super.key});
+  const AirConTimer({Key? key}) : super(key: key);
 
   @override
   State<AirConTimer> createState() => _AirConTimerState();
 }
 
 class _AirConTimerState extends State<AirConTimer> {
+  //Time Picker用の変数を定義
+  TimeOfDay selectedTime = TimeOfDay.now();
+
+  // 追加: 現在時刻を保持する変数
+  TimeOfDay nowTime = TimeOfDay.now();
+
+  // 追加: Timerを保持する変数
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 追加: 初期化時に現在時刻を1秒ごとに更新するTimerを設定
+    timer = Timer.periodic(
+      Duration(seconds: 1),
+      (Timer t) => setState(() {
+        nowTime = TimeOfDay.now();
+      }),
+    );
+  }
+
+  @override
+  void dispose() {
+    // 追加: Timerを破棄
+    timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +64,8 @@ class _AirConTimerState extends State<AirConTimer> {
               style: TextStyle(fontSize: 20.0),
             ),
             Text(
-              "23:30",
+              // 変更: 現在時刻を表示
+              '${nowTime.hour.toString().padLeft(2, "0")}:${nowTime.minute.toString().padLeft(2, "0")}',
               style: TextStyle(fontSize: 40.0),
             ),
             SizedBox(
@@ -45,15 +76,18 @@ class _AirConTimerState extends State<AirConTimer> {
               style: TextStyle(fontSize: 20.0),
             ),
             Text(
-              "6:00",
+              '${selectedTime.hour.toString().padLeft(2, "0")}:${selectedTime.minute.toString().padLeft(2, "0")}',
               style: TextStyle(fontSize: 40.0),
+            ),
+            ElevatedButton(
+              onPressed: () => _selectTime(context),
+              child: const Text('時刻選択'),
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20))),
             ),
             SizedBox(
               height: 50,
-            ),
-            Text(
-              "何時間後？",
-              style: TextStyle(fontSize: 20.0),
             ),
             Text(
               "6.5時間",
@@ -70,5 +104,19 @@ class _AirConTimerState extends State<AirConTimer> {
         ),
       ),
     );
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final TimeOfDay? picked = await showTimePicker(
+      context: context,
+      initialTime: selectedTime,
+      initialEntryMode: TimePickerEntryMode.input,
+    );
+
+    if (picked != null) {
+      setState(() {
+        selectedTime = picked;
+      });
+    }
   }
 }
